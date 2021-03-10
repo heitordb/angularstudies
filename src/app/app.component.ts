4b751955b7fc8a71fe718cdee72688a9297fa5de
Heitor Buzzatto <hdb_523@hotmail.com>
2021-03-09 21:01:07 -0300

    observables part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project';
   loadedFeature = 'recipe';
-
-  onNavigate(feature: string){
-    this.loadedFeature=feature;
-  }
 }
 
 
